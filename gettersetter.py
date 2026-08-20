5fd34ea373236ba9c9b5054391fa5f3e9f5f6801
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-20 18:13:29 +0530

    Getter Setter Implementation

---
+++ gettersetter.py
@@ -0,0 +1,16 @@
+class Tealeaf:
+    def __init__(self, age):
+        self._age = age
+    @property
+    def age(self):
+        return self._age + 2
+    @age.setter
+    def age(self, age):
+        if 1 <= age <= 5:
+            self._age = age
+        else:
+            raise ValueError("1 <= age <= 5")
+leaf = Tealeaf(2)
+print(leaf.age)
+leaf.age = 6
+print(leaf.age)
