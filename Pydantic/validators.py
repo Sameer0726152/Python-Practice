b5192a6af5bcdbd6e663743f91aa6d1402ea6e47
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-30 17:32:03 +0530

    Explored more validation

--- Pydantic/validators.py
+++ Pydantic/validators.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel, field_validator, model_validator
 class User(BaseModel):
-    name : str
-    @field_validator('name')
+    first_name : str
+    last_name : str
+    @field_validator('first_name')
     def name_len(cls, v):
         if len(v) < 4:
-            raise ValueError("Name is too small")
+            raise ValueError("First Name is too small")
+        return v
+    @field_validator('first_name', 'last_name')
+    def name_title(cls, v):
+        if not v.istitle():
+            raise ValueError("Names must be capitalised")
         return v
 class SignUp(BaseModel):
     real_pass : str
@@ -15,9 +21,28 @@ class SignUp(BaseModel):
             raise ValueError("PassWords dont match")
         return values
 
-user = {'name' : 'Sameer'}
+class emails(BaseModel):
+    email : str
+    @field_validator('email')
+    def normalize(cls, v):
+        return v.lower().strip()
+
+class Product(BaseModel):
+    price : float
+    @field_validator('price', mode = 'before')
+    def parse_price(cls, v):
+        if isinstance(v, str):
+            return float(v.replace('$', '').replace(',', '.'))
+        return v
+
+
+user = {'first_name' : 'Sameer', 'last_name' : 'Talekar'}
 sign = {'real_pass' : 'idkbro', 'con_pass' : 'idkbro'}
 u1 = User(**user)
 s1 = SignUp(**sign)
+e1 = emails(email = "    SamWr@gmIl.com  ")
+p1 = Product(price = "$44,44")
 print(u1)
 print(s1)
+print(e1)
+print(p1)
