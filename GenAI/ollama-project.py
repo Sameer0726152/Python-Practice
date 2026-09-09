f2be2710f65e5ed838cd83806ec19886ca274003
Sameer Talekar <sameertalekar2007@gmail.com>
2026-09-09 16:43:39 +0530

    Running qwen model locally using fastAPI

--- GenAI/ollama-project.py
+++ GenAI/ollama-project.py
@@ -1,5 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from ollama import Client
 app = FastAPI()
+client = Client(
+    host = "http://localhost:11434"
+)
 @app.get("/")
-async def root():
+def read_root():
     return {"message": "Hello World"}
+
+@app.post("/chat")
+def chat(
+        message : str = Body(..., description="The Message")
+):
+    response = client.chat(model = "qwen2.5-coder:3b", messages = [
+        {"role" : "user", "content" : message}
+    ])
+    return {"resonse" : response.message.content}
