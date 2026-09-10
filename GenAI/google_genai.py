2a4f2536b4f247277204ed65196a31bf4658ca95
Sameer Talekar <sameertalekar2007@gmail.com>
2026-09-11 00:27:55 +0530

    Configured gemini 3.7 flash API

--- GenAI/google_genai.py
+++ GenAI/google_genai.py
@@ -1,8 +1,12 @@
 from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 client = genai.Client(
-    api_key = "Your-API-Key"
+    api_key = os.getenv("GEMINI_API_KEY")
 )
 response = client.models.generate_content(
-    model = "gemini-3.6-flash", contents = "How do you feel?"
+    model = "gemini-3.7-flash", contents = "How do you feel?"
 )
 print(response.text)
